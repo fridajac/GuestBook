5f1f1162efc054a54a16f253a8a3ffb5c20d5fb8
fridajac <frida_jacobsson@live.se>
2022-03-04 15:22:01 +0100

    Implemented functionality for creating a new post

--- index.js
+++ index.js
@@ -1,7 +1,32 @@
 const form = document.querySelector("form");
 let nameInput = document.getElementById("name-input");
 let textInput = document.getElementById("text-input");
+const postSection = document.querySelector(".all-posts");
+createNewPost("Frida", "Lorem ipsum dolor sit amet");
+createNewPost(
+  "Victor",
+  "Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet"
+);
 
-form.addEventListener("submit", (e) => {
+form.addEventListener("Submit", (e) => {
   console.log("hello");
 });
+
+function createNewPost(name, text) {
+  const newPost = document.createElement("article");
+  newPost.classList.add("post");
+
+  const nameOutput = document.createElement("p");
+  const textOutput = document.createElement("p");
+  nameOutput.classList.add("name-output");
+  textOutput.classList.add("text-output");
+
+  const nodeName = document.createTextNode(name);
+  const nodeText = document.createTextNode(text);
+  nameOutput.appendChild(nodeName);
+  textOutput.appendChild(nodeText);
+
+  newPost.appendChild(nameOutput);
+  newPost.appendChild(textOutput);
+  postSection.appendChild(newPost);
+}
