6482c38de2e5bee262f53f63e297f2b7bcac341d
fridajac <frida_jacobsson@live.se>
2022-03-04 14:52:06 +0100

    Created simple index.js file

---
+++ index.js
@@ -0,0 +1,7 @@
+const form = document.querySelector("form");
+let nameInput = document.getElementById("name-input");
+let textInput = document.getElementById("text-input");
+
+form.addEventListener("submit", (e) => {
+  console.log("hello");
+});
